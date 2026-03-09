bf22eb04e638589017c41749692e0f1125fbdfb1
skyedurano <ashleeeskye@gmail.com>
2026-03-09 21:37:25 +0800

    backend structure creation

---
+++ backend/src/test/java/edu/cit/durano/foodcloud/FoodcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.durano.foodcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
