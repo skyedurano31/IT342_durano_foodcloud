65d62c9c7cbe62e94f073276cf74c4bad76a7db2
skyedurano <ashleeeskye@gmail.com>
2026-04-03 14:39:57 +0800

    added findbygooglgeid in user repository

--- backend/src/main/java/edu/cit/durano/foodcloud/repository/UserRepository.java
+++ backend/src/main/java/edu/cit/durano/foodcloud/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    Optional<User> findByGoogleId(String googleId);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
