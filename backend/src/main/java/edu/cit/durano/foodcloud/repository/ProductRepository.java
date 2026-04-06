e46f5c19127fdac63ab7f9e07cb07b9fe87778b9
skyedurano <ashleeeskye@gmail.com>
2026-04-06 13:14:49 +0800

    product repository

---
+++ backend/src/main/java/edu/cit/durano/foodcloud/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package edu.cit.durano.foodcloud.repository;
+
+import edu.cit.durano.foodcloud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
