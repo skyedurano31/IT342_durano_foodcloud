cff9af9bd1826dc692c4294fe465adc0c2e67416
skyedurano <ashleeeskye@gmail.com>
2026-04-10 20:07:17 +0800

    customized cart repo

--- backend/src/main/java/edu/cit/durano/foodcloud/repository/CartRepository.java
+++ backend/src/main/java/edu/cit/durano/foodcloud/repository/CartRepository.java
@@ -1,7 +1,12 @@
 package edu.cit.durano.foodcloud.repository;
 
 import edu.cit.durano.foodcloud.entity.Cart;
+import edu.cit.durano.foodcloud.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    Optional<Cart> findByUser(User user);
+    Optional<Cart> findByUserId(Long userId);
 }
