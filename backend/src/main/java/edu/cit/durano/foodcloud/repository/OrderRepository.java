08af88734d70a64452f7e535c6035623d5e7d68b
skyedurano <ashleeeskye@gmail.com>
2026-04-10 20:07:39 +0800

    customized order repo

--- backend/src/main/java/edu/cit/durano/foodcloud/repository/OrderRepository.java
+++ backend/src/main/java/edu/cit/durano/foodcloud/repository/OrderRepository.java
@@ -1,7 +1,12 @@
 package edu.cit.durano.foodcloud.repository;
 
 import edu.cit.durano.foodcloud.entity.Order;
+import edu.cit.durano.foodcloud.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findByUser(User user);
+    List<Order> findByUserId(Long userId);
 }
