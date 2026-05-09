79668bbb0b799165c2b6d77ff7ae831fd79073a9
skyedurano <ashleeeskye@gmail.com>
2026-05-09 23:48:32 +0800

    modified the test file

--- backend/src/test/java/edu/cit/durano/foodcloud/customer/CustomerFlowTests.java
+++ backend/src/test/java/edu/cit/durano/foodcloud/customer/CustomerFlowTests.java
@@ -1,4 +1,23 @@
 package edu.cit.durano.foodcloud.customer;
 
-public class CustomerFlowTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFlowTests {
+
+    @Test
+    void testGetAllProducts_ReturnsList() {
+        assert true;
+    }
+
+    @Test
+    void testCreateProduct_Success() {
+        assert true;
+    }
+
+    @Test
+    void testDeleteProduct_Success() {
+        assert true;
+    }
 }
